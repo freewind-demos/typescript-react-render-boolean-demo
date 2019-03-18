2acef7d2acabeaf95fb3b4487b99ad10884c6e7b
freewind <nowindlee@gmail.com>
2019-03-18 15:10:24 +0800

    demo

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-TypeScript React Hello World Demo
+TypeScript React Render Boolean Demo
 =================================
+
+在react中，`boolean`是不会render出来的，所以可以让我们在根据条件展示某些component的时候，语法非常简单
 
 ```
 npm install
 npm run demo
 ```
-
-It will open page on browser automatically.
--- src/hello.tsx
+++ src/hello.tsx
@@ -2,11 +2,17 @@
 import React from 'react'
 
 class Hello extends React.Component {
-    render() {
-        return <div>
-            <h1>Hello React</h1>
-        </div>
-    }
+  render() {
+    return <div>
+      <h1>Hello React</h1>
+      <ul>
+        <li>{true}</li>
+        <li>{false}</li>
+        <li>{true && <div>TRUE</div>}</li>
+        <li>{false && <div>FALSE</div>}</li>
+      </ul>
+    </div>
+  }
 }
 
 export default Hello
